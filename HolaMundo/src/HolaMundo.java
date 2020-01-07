f1535c242013894f770912829010d2d20733c33a
pedro_coleta <pedro@hotmail.com>
2020-01-07 18:14:58 +0100

    2 Cambio Consola

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -2,8 +2,9 @@
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println(" - HolaMundo desde Eclipse");
-		System.out.println("HolaMundo desde Local");
+		System.out.println("1 - cambio desde Consola");
+		System.out.println("1 - cambio desde Eclipse");
+		System.out.println("2 - cambio desde Consola");
 	}
 
 }
